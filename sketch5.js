f542d765bd3f6f2661813e8377747b9fda5eb8fa
Rebecca Lieberman <rebeccalieberman@gmail.com>
2016-02-13 23:07:04 -0500

    adding files

--- sketch5.js
+++ sketch5.js
@@ -1,47 +1,49 @@
 
 //Loads cities from cities.json file, from https://github.com/deremer/Cities
-var rolloverText; //global var cityText
+//Uses this to do CSS for responsive photo grid https://css-tricks.com/seamless-responsive-photo-grid/
+var rolloverText; //global var called rolloverText
 
 
 
-function setup() {
+function setup() { //using p5 to load the json file
 
   noCanvas();
 
-  //STEPS
-  //load the cities.json file
-  //for every city in the cities.json file
-  //get the image from the image directory based on the city name
-  //display the image as a DOM element on the page
-  //pass it to the rolloverCity method to display the name of the city that the mouse is on
 
-  rolloverText = createDiv(' '); //create one P instead of multiple so you can only have one showing at a time, no need for mouseOut
+  //create an empty div for the rollover text
+  rolloverText = document.createElement('div');
+  rolloverText.setAttribute("class", "rollover");   //add class rollover
   
-  loadCities();
+  loadCities(); //call loadCities function
 
 }
 
-//function to load the cities.json file
-function loadCities() {
+//STEPS
+//load the cities.json file
+//for every city in the cities.json file
+//get the image from the image directory based on the city name
+//display the image as a DOM element on the page
+//pass it to the rolloverCity method to display the name of the city that the mouse is on
 
-  loadJSON('cities.json', function(cities){
-    for (var i = 0; i < cities.length; i ++) {
-      // console.log(cities[i]); //cities[i] is an object for each city
+function loadCities() { //function to load the cities.json file
+
+  loadJSON('cities.json', function(cities){ 
+    for (var i = 0; i < cities.length; i++) {
       var cityName = cities[i].city;
-      insertImage(cityName);
+      insertImage(cityName); //callback to insert the image for each city name
     }
   });
 }
 
+
 function insertImage(city) {
 
-  //create a container div that will hold the image
-  var containerDiv = document.createElement('div');
+  var containerDiv = document.createElement('div');   //create a container div that will hold the image
   containerDiv.setAttribute('class', 'container'); //give it the class container
 
-  //create the image
+  //create the image from the folder of images
   var img = document.createElement('img'); //create an empty image element
-  img.src = 'images/' + encodeURIComponent(city) + '.jpg';
+  img.src = 'images/' + encodeURIComponent(city) + '.jpg'; //set the source to the cityname
 
   //once the image has loaded
   img.addEventListener('load', function(){ 
@@ -67,23 +69,18 @@ function rolloverCity (imgElt, cityName, containerDiv) {
       var imgY = imgElt.offsetTop;
       console.log(imgX, imgY, cityName);
 
-      // var overlayDiv = document.createElement('div'); //create a div that will be a dark overlay
-      // overlayDiv.setAttribute('class', 'overlay'); 
+      imgElt.setAttribute('class', 'imgFilter'); 
 
-      // //POSITIONING NOT WORKING
-      // overlayDiv.style.left(imgX); 
-      // overlayDiv.style.top(imgY); 
-
-
-     
 
       //change the innerHTML and positioning of the rollover text
+      rolloverText.innerHTML = cityName;
 
-      rolloverText.html(cityName);
-      rolloverText.position(imgX, imgY);
-      containerDiv.appendChild(rolloverText); //NOT WORKING
-      
-   
+      containerDiv.appendChild(rolloverText); 
+      rolloverText.style.position = "absolute";
+      rolloverText.style.left = imgX;
+      rolloverText.style.left = imgY;
+  
+
 
    }
 
